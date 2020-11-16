c39592d1a1f8b152ebf8843f54a431b846c1b856
J Perrin <perrinjack96@gmail.com>
2020-11-16 13:31:02 +0000

    Currecy ex data retrieved from api

--- Components/dashboard.js
+++ Components/dashboard.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
+import apiKey from '../apiKey.js';
+import axios from 'axios';
+import { StyleSheet, View } from 'react-native';
 
-class Dashboard extends Component {
+export class Dashboard extends React.Component {
+  state = {
+    data: null,
+    loaded: false,
+  };
+
+  componentDidMount() {
+    this.handleSubmit();
+  }
+
+  handleSubmit = () => {
+    axios
+      .get(
+        `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=JPY&apikey=7ECA0DL4Z1Q6NGD1`
+      )
+      .then((res) => {
+        // console.log(res);
+        this.setState({
+          data: [res.data],
+        });
+        this.setState({
+          loaded: true,
+        });
+        // console.log(res.data['Realtime Currency Exchange Rate']["9. Ask Price"]);
+      });
+  };
+
   render() {
-    return <Text>Hello, I am your cat!</Text>;
+    return this.state.loaded && <View></View>;
   }
 }
-
-export default Dashboard;
